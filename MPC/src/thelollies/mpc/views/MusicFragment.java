e57c7667b6fdf15f2b2e2039aded91ced0f68a2a
thelollies <rorystephenson@gmail.com>
2014-05-14 09:41:31 +1200

    Extracted view adapters. Optimisations and renaming to better reflect what the Fragment does

--- MPC/src/thelollies/mpc/views/MusicFragment.java
+++ MPC/src/thelollies/mpc/views/MusicFragment.java
@@ -4,11 +4,13 @@ import java.io.Serializable;
 import java.util.List;
 
 import mpc.MPCAlbum;
+import mpc.MPCArtist;
 import mpc.MPCQuery;
 import mpc.MPCSong;
 import thelollies.mpc.R;
 import thelollies.mpc.database.SongDatabase;
 import thelollies.mpc.models.ListState;
+import thelollies.mpc.models.MPCSingleTypeAdapter;
 import thelollies.mpc.views.ViewPagerAdapter.TabType;
 import android.content.Context;
 import android.os.Bundle;
@@ -16,11 +18,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-
-import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.app.SherlockListFragment;
 
 public class MusicFragment extends SherlockListFragment implements MPCFragment{
@@ -108,128 +106,13 @@ public class MusicFragment extends SherlockListFragment implements MPCFragment{
 		return inflater.inflate(R.layout.fragment_music, container, false);
 	}
 
-
-
-	/**
-	 * Adapter for the listView which displays MPCSongs.
-	 * @author thelollies
-	 *
-	 */
-	private class MPCSongAdapter extends ArrayAdapter<MPCSong> {
-
-		private List<MPCSong> songs;
-		private int layout;
-
-		public MPCSongAdapter(Context context, int textViewResourceId, List<MPCSong> items) {
-			super(context, textViewResourceId, items);
-			this.songs = items;
-			this.layout = textViewResourceId;
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View v = convertView;
-			if (v == null) {
-				LayoutInflater vi = (LayoutInflater)getSherlockActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-				v = vi.inflate(layout, null);
-			}
-			MPCSong song = songs.get(position);
-			if (song != null) {
-				TextView tt = (TextView) v.findViewById(R.id.toptext);
-				TextView bt = (TextView) v.findViewById(R.id.bottomtext);
-				if (tt != null) {
-					tt.setText(song.title);                            }
-				if(bt != null){
-					bt.setText(song.artist);
-				}
-			}
-			return v;
-		}
-	}
-
-	/**
-	 * Adapter for the listView which displays MPCAlbums.
-	 * @author thelollies
-	 *
-	 */
-	private class MPCAlbumAdapter extends ArrayAdapter<MPCAlbum> {
-
-		private List<MPCAlbum> albums;
-		private int layout;
-
-		public MPCAlbumAdapter(Context context, int textViewResourceId, List<MPCAlbum> items) {
-			super(context, textViewResourceId, items);
-			this.albums = items;
-			this.layout = textViewResourceId;
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View v = convertView;
-			if (v == null) {
-				LayoutInflater vi = (LayoutInflater)getSherlockActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-				v = vi.inflate(layout, null);
-			}
-			MPCAlbum album = albums.get(position);
-			if (album != null) {
-				TextView tt = (TextView) v.findViewById(R.id.toptext);
-				TextView bt = (TextView) v.findViewById(R.id.bottomtext);
-				if (tt != null) {
-					tt.setText(album.title);                            }
-				if(bt != null){
-					bt.setText(album.artist);
-				}
-			}
-			return v;
-		}
-	}
-
-	/**
-	 * Adapter for the listView which displays artist names.
-	 * @author thelollies
-	 *
-	 */
-	private class MPCArtistAdapter extends ArrayAdapter<String> {
-
-		private List<String> artists;
-		private int layout;
-
-		public MPCArtistAdapter(Context context, int textViewResourceId, List<String> items) {
-			super(context, textViewResourceId, items);
-			this.artists = items;
-			this.layout = textViewResourceId;
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View v = convertView;
-			if (v == null) {
-				LayoutInflater vi = (LayoutInflater)getSherlockActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-				v = vi.inflate(layout, null);
-			}
-			String album = artists.get(position);
-			if (album != null) {
-				TextView tt = (TextView) v.findViewById(R.id.toptext);
-				TextView bt = (TextView) v.findViewById(R.id.bottomtext);
-				if (tt != null) {
-					tt.setText(album);                            }
-				if(bt != null){
-					bt.setText("Albums");
-				}
-			}
-			return v;
-		}
-	}
-
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		Object o = l.getItemAtPosition(position);
 
-		SherlockFragmentActivity activity = getSherlockActivity();
-
 		if(o instanceof MPCSong){
 			if(!currentState.query.equals(TabContainer.playing)){
-				List<MPCSong> songs = new SongDatabase(activity).processSongQuery(currentState.query);
+				List<MPCSong> songs = songDatabase.processSongQuery(currentState.query);
 				TabContainer.mpc.enqueSongs(songs);
 				TabContainer.playing = currentState;
 			}
@@ -240,16 +123,16 @@ public class MusicFragment extends SherlockListFragment implements MPCFragment{
 
 			currentState.setY(getListView().getFirstVisiblePosition());
 			if(album.isAll()){
-				currentState = new ListState(currentState, new MPCQuery(MPCQuery.SONGS_BY_ARTIST, album.artist));
+				currentState = new ListState(currentState, new MPCQuery(MPCQuery.SONGS_BY_ARTIST, album));
 			}
 			else{
-				currentState = new ListState(currentState, new MPCQuery(MPCQuery.SONGS_BY_ALBUM_ARTIST, album.artist, album.title));
+				currentState = new ListState(currentState, new MPCQuery(MPCQuery.SONGS_BY_ALBUM_ARTIST, album));
 			}
 
 			refreshList();
 		}
-		else if(o instanceof String){
-			String artist = (String) o;
+		else if(o instanceof MPCArtist){
+			MPCArtist artist = (MPCArtist) o;
 			currentState.setY(getListView().getFirstVisiblePosition());
 
 			currentState = new ListState(currentState, new MPCQuery(MPCQuery.ALBUMS_BY_ARTIST, artist));
@@ -264,25 +147,26 @@ public class MusicFragment extends SherlockListFragment implements MPCFragment{
 		int type = currentState.query.getType();
 		Context context = getSherlockActivity();
 
-
 		if(type == MPCQuery.ALL_SONGS || 
 				type == MPCQuery.SONGS_BY_ALBUM_ARTIST ||
 				type == MPCQuery.SONGS_BY_ARTIST){
-			List<MPCSong> songs = new SongDatabase(context).processSongQuery(currentState.query);
-			MPCSongAdapter adapter = new MPCSongAdapter(context, R.layout.row_song, songs);
+			List<MPCSong> songs = songDatabase.processSongQuery(currentState.query);
+			MPCSingleTypeAdapter<MPCSong> adapter = 
+					new MPCSingleTypeAdapter<MPCSong>(context, R.layout.row_song, songs);
 			setListAdapter(adapter);
 		}
 		else if(type == MPCQuery.ALL_ARTISTS){
-			List<String> artists = new SongDatabase(context).processArtistQuery(currentState.query);
-			MPCArtistAdapter adapter = new MPCArtistAdapter(context, R.layout.row_artist, artists);
+			List<MPCArtist> artists = new SongDatabase(context).processArtistQuery(currentState.query);
+			MPCSingleTypeAdapter<MPCArtist> adapter = 
+					new MPCSingleTypeAdapter<MPCArtist>(context, R.layout.row_artist, artists);
 			setListAdapter(adapter);
 		}
 		else{
 			List<MPCAlbum> albums = new SongDatabase(context).processAlbumQuery(currentState.query);
-			MPCAlbumAdapter adapter = new MPCAlbumAdapter(context, R.layout.row_album, albums);
+			MPCSingleTypeAdapter<MPCAlbum> adapter = 
+					new MPCSingleTypeAdapter<MPCAlbum>(context, R.layout.row_album, albums);
 			setListAdapter(adapter);
 		}
-
 	}
 
 	public boolean navigateUp(){
